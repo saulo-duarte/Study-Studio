2548d4fa4e3d08f6332745f6723d2c8f3391bc8d
saulo-duarte <saulolduarte@outlook.com>
2024-12-24 19:41:01 -0300

    chore: set up main entry point and lib configuration

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,33 +1,46 @@
-mod db;
-mod utils;
-use db::{check_user_and_redirect_command, create_user, initialize_database};
-use utils::path::get_database_path;
+use rusqlite::{Connection, Result};
+use std::sync::{Arc, Mutex};
+use std::path::Path;
+
+pub mod db;
+pub mod commands;
+
+use db::run_migrations;
+use commands::user_commands;
+
+pub struct AppState {
+    pub db_conn: Arc<Mutex<Connection>>, 
+}
+
+impl AppState {
+    pub fn new(db_path: &str) -> Result<Self> {
+        if !Path::new(db_path).exists() {
+            println!("Banco de dados será criado em: {}", db_path);
+        }
+
+        let conn = Connection::open(db_path)?;
+
+        run_migrations(&conn)?;
+
+        Ok(Self {
+            db_conn: Arc::new(Mutex::new(conn)),
+        })
+    }
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let db_path = "./app.db";
+
+    let app_state = AppState::new(&db_path).expect("Falha ao inicializar o AppState");
+
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::new().build())
+        .manage(app_state)
         .invoke_handler(tauri::generate_handler![
-            create_user,
-            initialize_database,
-            check_user_and_redirect_command,
+            user_commands::create_user, 
+            user_commands::list_all_users,
+            user_commands::check_if_there_is_active_user_status,
         ])
-        .setup(|_app| {
-            let db_path = get_database_path();
-            println!("Database path: {:?}", db_path);
-
-            // Ensure the database file and directory exist
-            std::fs::create_dir_all(db_path.parent().unwrap())
-                .expect("Failed to create database directory");
-
-            // Initialize the database during setup
-            match initialize_database() {
-                Ok(_) => println!("Database initialized successfully during setup"),
-                Err(e) => eprintln!("Error initializing database during setup: {}", e),
-            }
-
-            Ok(())
-        })
         .run(tauri::generate_context!())
-        .expect("Failed to run Tauri application");
+        .expect("Erro ao rodar a aplicação Tauri");
 }
