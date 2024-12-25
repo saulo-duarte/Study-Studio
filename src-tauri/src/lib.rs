aabb2fbf7e3f20aac7ca75d610d3a608f08525fd
saulo-duarte <saulolduarte@outlook.com>
2024-12-25 18:37:27 -0300

    chore(dependencies): add dependencies

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,16 +6,16 @@ pub mod db;
 pub mod commands;
 
 use db::run_migrations;
-use commands::user_commands;
+use commands::{create_user_command, check_if_there_is_active_user_status_command};
 
 pub struct AppState {
-    pub db_conn: Arc<Mutex<Connection>>, 
+    pub db_conn: Arc<Mutex<Connection>>,
 }
 
 impl AppState {
     pub fn new(db_path: &str) -> Result<Self> {
         if !Path::new(db_path).exists() {
-            println!("Banco de dados será criado em: {}", db_path);
+            println!("Database at: {}", db_path);
         }
 
         let conn = Connection::open(db_path)?;
@@ -25,6 +25,10 @@ impl AppState {
         Ok(Self {
             db_conn: Arc::new(Mutex::new(conn)),
         })
+    }
+
+    pub fn db_conn(&self) -> Arc<Mutex<Connection>> {
+        Arc::clone(&self.db_conn)
     }
 }
 
@@ -37,9 +41,9 @@ pub fn run() {
     tauri::Builder::default()
         .manage(app_state)
         .invoke_handler(tauri::generate_handler![
-            user_commands::create_user, 
-            user_commands::list_all_users,
-            user_commands::check_if_there_is_active_user_status,
+            create_user_command,
+            check_if_there_is_active_user_status_command,
+
         ])
         .run(tauri::generate_context!())
         .expect("Erro ao rodar a aplicação Tauri");
