c6d869ce2aa16aab19325546dc80f0d1df7ac2cc
saulo-duarte <saulolduarte@outlook.com>
2025-01-01 18:36:03 -0300

    chore: update dependencies

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,8 @@
 use rusqlite::{Connection, Result};
 use std::sync::{Arc, Mutex};
-use std::path::Path;
+use std::fs;
+use std::path::PathBuf;
+use std::io;
 
 pub mod db;
 pub mod commands;
@@ -13,9 +15,17 @@ pub struct AppState {
 }
 
 impl AppState {
-    pub fn new(db_path: &str) -> Result<Self> {
-        if !Path::new(db_path).exists() {
-            println!("Database at: {}", db_path);
+    pub fn new(db_path: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let db_path = PathBuf::from(db_path);
+
+        // Certifique-se de que o diretório exista
+        if let Some(parent) = db_path.parent() {
+            // Usando map_err para mapear o erro de I/O para um erro genérico
+            fs::create_dir_all(parent).map_err(|e| Box::new(e) as Box<dyn std::error::Error>)?;
+        }
+
+        if !db_path.exists() {
+            println!("Database at: {:?}", db_path);
         }
 
         let conn = Connection::open(db_path)?;
@@ -34,16 +44,20 @@ impl AppState {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let db_path = "./app.db";
+    // Determinar o caminho da database
+    let mut db_path = dirs::config_dir().unwrap_or_else(|| PathBuf::from("."));
+    db_path.push("study-studio");
+    db_path.push("app.db");
 
-    let app_state = AppState::new(&db_path).expect("Falha ao inicializar o AppState");
+    let db_path_str = db_path.to_string_lossy().to_string();
+
+    let app_state = AppState::new(&db_path_str).expect("Falha ao inicializar o AppState");
 
     tauri::Builder::default()
         .manage(app_state)
         .invoke_handler(tauri::generate_handler![
             create_user_command,
             check_if_there_is_active_user_status_command,
-
         ])
         .run(tauri::generate_context!())
         .expect("Erro ao rodar a aplicação Tauri");
