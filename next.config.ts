6b9ad4d5d282d3e4e6402a654aba3687bf39c5d0
saulo-duarte <saulolduarte@outlook.com>
2025-01-03 10:19:53 -0300

    feat(workers): configure pdf workers

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,29 @@ const nextConfig: NextConfig = {
   distDir: "out",
   images: {
     domains: ["i.imgur.com"],
+    unoptimized: true,
   },
-  output: 'export',
+  output: "export",
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      type: 'javascript/auto',
+    });
+
+    config.module.rules.push({
+      test: /pdf\.worker\.min\.mjs$/,
+      use: {
+        loader: 'file-loader',
+      },
+    });
+
+    if (!config.resolve.alias) {
+      config.resolve.alias = {};
+    }
+    config.resolve.alias.canvas = false;
+
+    return config;
+  },
 };
 
 export default nextConfig;
